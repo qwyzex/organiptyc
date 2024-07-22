ceb28187dc212297bbae40189031cf7b9c3eb630
qwyzex <qwyzex@gmail.com>
2024-07-22 20:28:05 +0700

    FIX: Add NEXT_PUBLIC TO ALL ENV VARIABLES

--- firebase.ts
+++ firebase.ts
@@ -7,34 +7,13 @@ import { FirebaseStorage, getStorage } from "firebase/storage";
 console.log("PROCESS API KEY:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
 
 const firebaseConfig = {
-    apiKey:
-        process.env.NODE_ENV === "development"
-            ? process.env.NEXT_PUBLIC_FIREBASE_API_KEY
-            : process.env.FIREBASE_API_KEY,
-    authDomain:
-        process.env.NODE_ENV === "development"
-            ? process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN
-            : process.env.FIREBASE_AUTH_DOMAIN,
-    projectId:
-        process.env.NODE_ENV === "development"
-            ? process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
-            : process.env.FIREBASE_PROJECT_ID,
-    storageBucket:
-        process.env.NODE_ENV === "development"
-            ? process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET
-            : process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId:
-        process.env.NODE_ENV === "development"
-            ? process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID
-            : process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId:
-        process.env.NODE_ENV === "development"
-            ? process.env.NEXT_PUBLIC_FIREBASE_APP_ID
-            : process.env.FIREBASE_APP_ID,
-    measurementId:
-        process.env.NODE_ENV === "development"
-            ? process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
-            : process.env.FIREBASE_MEASUREMENT_ID,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
