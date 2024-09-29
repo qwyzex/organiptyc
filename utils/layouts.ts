b29ebe9c2139854a5edae41d32817c8b8a494968
qwyzex <qwyzex@gmail.com>
2024-09-29 12:26:54 +0700

    Fix: remove orgLayut and HomeLayout, replaced with DefaultLayout

--- utils/layouts.ts
+++ utils/layouts.ts
@@ -1,13 +1,7 @@
 import LandingLayout from "@/components/layouts/LandingLayout";
 import AuthLayout from "@/components/layouts/AuthLayout";
-import HomeLayout from "@/components/layouts/HomeLayout";
-import OrgLayout from "@/components/layouts/OrgLayout";
 import EmptyLayout from "@/components/layouts/EmptyLayout";
 import DefaultLayout from "@/components/layouts/DefaultLayout";
-import { useContext } from "react";
-import { UserContext } from "@/context/UserContext";
-
-import { useEffect } from "react";
 
 const getLayoutByRoute = (pathname: string) => {
     if (
