7f04fb3763aaa3bc6fed175cc4110fa013656b82
qwyzex <qwyzex@gmail.com>
2024-10-03 01:30:06 +0700

    Clean createLog.ts

--- function/createLog.ts
+++ function/createLog.ts
@@ -1,19 +1,18 @@
 import { db } from "@/firebase";
 import { collection, addDoc, Timestamp } from "firebase/firestore";
 
-// export type logDoc = {
-//     orgId: string;
-//     userId: string;
-//     action: any;
-// };
-
 /**
  * Function to create a log entry for an organization
  * @param {string} orgId - The ID of the organization
  * @param {string} userId - The ID of the user performing the action
  * @param {string} action - A description of the action performed
  */
-const createLog = async (orgId: string, userId: string, action: any, photoURL: string) => {
+const createLog = async (
+    orgId: string,
+    userId: string,
+    action: any,
+    photoURL: string
+) => {
     try {
         const logRef = collection(db, `organizations/${orgId}/logs`);
         await addDoc(logRef, {
