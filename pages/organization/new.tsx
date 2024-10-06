6deb79bf548aefff3153bf662e64aae7a2d32d35
qwyzex <qwyzex@gmail.com>
2024-10-06 18:02:25 +0700

    New organization page restyle

--- pages/organization/new.tsx
+++ pages/organization/new.tsx
@@ -1,25 +1,51 @@
-import {useRouter} from "next/router"
+import { useRouter } from "next/router";
+import styles from "@/styles/organization/NewOrg.module.sass";
+import { Button } from "@mui/material";
+import {
+    ArrowBackIosNew,
+    ArrowForwardIos,
+    Create,
+    DomainAdd,
+    JoinFull,
+} from "@mui/icons-material";
 
 export default function New() {
-    const router = useRouter()
+    const router = useRouter();
 
     return (
-        <div>
-            <div>
-                <button onClick={() => router.back()}>
-                    <h2>BACK</h2>
-                </button>
-            </div>
-            <div>
-                <button onClick={() => router.push("/organization/join")}>
-                    <h2>JOIN</h2>
-                </button>
-            </div>
-            <div>
-                <button onClick={() => router.push("/organization/create")}>
-                    <h2>CREATE</h2>
-                </button>
-            </div>
+        <div className={styles.container}>
+            <header>
+                <Button
+                    className="btn-compact"
+                    onClick={() => router.replace(`/organization`)}
+                >
+                    <ArrowBackIosNew />
+                </Button>
+                <h1>Engage in an Organization</h1>
+            </header>
+            <main>
+                <div onClick={() => router.push("/organization/join")}>
+                    <JoinFull />
+                    <h2>Join</h2>
+                    <p className="dim">
+                        Join an existing organization, trough invite link.
+                    </p>
+                    <div className={styles.arrowContainer}>
+                        <ArrowForwardIos fontSize="small" />
+                    </div>
+                </div>
+                <div onClick={() => router.push("/organization/create")}>
+                    <DomainAdd />
+                    <h2>Create</h2>
+                    <p className="dim">
+                        Create an organization to get started. You can manage
+                        everything in it.
+                    </p>
+                    <div className={styles.arrowContainer}>
+                        <ArrowForwardIos fontSize="small" />
+                    </div>
+                </div>
+            </main>
         </div>
     );
 }
